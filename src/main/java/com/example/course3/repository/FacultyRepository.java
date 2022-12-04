ff2fee5bc17b84798743415b278d13626641c75e
FickleIlya <fickle041@gmail.com>
2022-12-04 16:53:27 +0300

    Revert "init 3.3"
    This reverts commit 95a43be45a9017bd5430401e476af25476ad827e.

--- src/main/java/com/example/course3/repository/FacultyRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.course3.repository;
-
-import com.example.course3.model.Faculty;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FacultyRepository extends JpaRepository<Faculty, Long> {
-
-}
