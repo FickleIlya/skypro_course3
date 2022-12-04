13e091afc925cd6c1b8c4983d755a30700b380da
FickleIlya <fickle041@gmail.com>
2022-12-04 17:06:25 +0300

    init

---
+++ src/main/java/com/example/skyprocourse3/SkyproCourse3Application.java
@@ -0,0 +1,13 @@
+package com.example.skyprocourse3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproCourse3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproCourse3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/skyprocourse3/SkyproCourse3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skyprocourse3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyproCourse3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
