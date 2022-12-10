37dcd788b58b5833f242d42c85efa59d421c03b0
FickleIlya <fickle041@gmail.com>
2022-12-10 18:28:03 +0300

    change table name in scripts

--- scripts.sql
+++ scripts.sql
@@ -1,16 +1,16 @@
 -- Получить всех студентов,
 -- возраст которых находится между 10 и 20
 -- (можно подставить любые числа, главное, чтобы нижняя граница была меньше верхней).
-select * from students where age between 10 and 20;
+select * from student where age between 10 and 20;
 
 -- Получить всех студентов, но отобразить только список их имен
-select name from students;
+select name from student;
 
 -- Получить всех студентов, у которых в имени присутствует буква «О» (или любая другая)
-select * from students where name like '%о%';
+select * from student where name like '%о%';
 
 -- Получить всех студентов, у которых возраст меньше идентификатора.
-select * from students where age < id;
+select * from student where age < id;
 
 -- Получить всех студентов упорядоченных по возрасту.
-select * from students order by age;
+select * from student order by age;
